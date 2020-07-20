857b6405adb50b65f1e24258dd2498255ec6b0f7
zdflower <zdflower@users.noreply.github.com>
2020-07-19 23:31:51 -0300

    Manejo de eventos de conección, error, desconección y cierre de la base de datos.

--- helpers/dbconnect.js
+++ helpers/dbconnect.js
@@ -12,13 +12,8 @@ try {
   console.error(err);
 }
 
-mongoose.connection.on('error', err => {
-  console.error(err);
-});
-
-
 /* 
-Otra forma, que se usa en https://www.youtube.com/watch?v=JLwwQMU6Ru0:
+Otra forma, que se usa en https://www.youtube.com/watch?v=JLwwQMU6Ru0 :
 
 mongoose.connect(MONGODB_CONNECTION_STRING, 
   	{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
@@ -30,3 +25,31 @@ mongoose.connect(MONGODB_CONNECTION_STRING,
   	})
 
 */
+
+// manejadores de distintos eventos, por ej. 
+// que mongoose se conecte
+
+mongoose.connection.on('connected', () => {
+	console.log('Mongoose connected to db.');
+})
+
+// que suceda algún error después de haberse conectado
+mongoose.connection.on('error', err => {
+  console.error(err);
+});
+
+// que se desconecte
+mongoose.connection.on('disconnected', () => {
+	console.log('Mongoose connection is disconnected');
+});
+
+
+// Según https://www.youtube.com/watch?v=JLwwQMU6Ru0 es necesario asegurarse de cerrar
+// la conección y para eso hay que utilizar un manejador para el evento SIGINT
+// en el objeto process, (este evento se dispara al ejecutar ctrl+c 
+// para cerrar el servidor en el terminal):
+
+process.on('SIGINT', async () => {
+	await mongoose.connection.close();
+	process.exit(0); // cerrar la aplicación con un código numérico
+});
